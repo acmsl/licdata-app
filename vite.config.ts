123ee34cae4b8646114fd9690915aa20bc76d7e2
Jose San Leandro <jose@io.builders>
2023-03-11 18:22:56 +0100

    Attempt to make solid-start work on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import netlify from "solid-start-netlify";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: netlify({ edge: true })
+    })
+  ],
+  server: {
+    port: 3001,
+  },
+  build: {
+    target: 'esnext',
+  },
 });
